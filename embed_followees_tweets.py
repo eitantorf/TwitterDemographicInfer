36e43165a0cf737e20d1c66bb5407311b439bd29
eitantorf <eitantorf@gmail.com>
2022-02-20 10:26:38 +0200

    change spark start and end frequency

--- embed_followees_tweets.py
+++ embed_followees_tweets.py
@@ -18,49 +18,53 @@ findspark.os.environ["PYSPARK_PYTHON"]=   "./environment/bin/python"
 start_date = datetime.datetime(2020, 8, 1)
 end_date = datetime.datetime(2020, 11, 30)
 
-def run_spark_embedding(start_dt, end_dt):
-    start_dt = start_dt.strftime("%Y-%m-%d")
-    end_dt = end_dt.strftime("%Y-%m-%d")
+def run_spark_embedding(start_dt, num_days=1):
     with SparkSession.builder.appName(f"t_embed_{start_dt.replace('-','')}")\
                             .config("spark.archives", "onnx_conda_env.tar.gz#environment")\
                             .config("spark.sql.execution.arrow.enabled", "true")\
                             .getOrCreate() as spark:
         logging.info(f"Spark started")
-        spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", "8")
-        followee_uids = spark.read.parquet("/user/etorf/panel/panel_sample_final_followee_uids")
-        followee_uids = followee_uids.withColumn('user_id_bucket', followee_uids.followee_uid % 40)
-        followee_uids = followee_uids.withColumn("user_id_bucket", followee_uids.user_id_bucket.cast('string'))
+        for day in range(num_days):
+            dt = start_dt.strftime("%Y-%m-%d") # convert to string
+            for i in [(0,19), (20,39)]: # further split user_bucket to two to shorten each cycle
+                spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", "8")
+                followee_uids = spark.read.parquet("/user/etorf/panel/panel_sample_final_followee_uids")
+                followee_uids = followee_uids.withColumn('user_id_bucket', followee_uids.followee_uid % 40)
+                followee_uids = followee_uids.withColumn("user_id_bucket", followee_uids.user_id_bucket.cast('string'))
+                followee_uids = followee_uids.filter(f"user_id_bucket >= {str(i[0])} AND user_id_bucket <= {str(i[1])}")
 
-        tokenizer = BertTokenizer.from_pretrained('DeepPavlov/bert-base-cased-conversational')
-        model = BertModel.from_pretrained('DeepPavlov/bert-base-cased-conversational', return_dict=True)
-        bc_tokenizer = spark.sparkContext.broadcast(tokenizer)
-        bc_model = spark.sparkContext.broadcast(model)
+                tokenizer = BertTokenizer.from_pretrained('DeepPavlov/bert-base-cased-conversational')
+                model = BertModel.from_pretrained('DeepPavlov/bert-base-cased-conversational', return_dict=True)
+                bc_tokenizer = spark.sparkContext.broadcast(tokenizer)
+                bc_model = spark.sparkContext.broadcast(model)
 
-        @pandas_udf(T.ArrayType(T.DoubleType()))
-        def embed_batch_udf(texts: pd.Series) -> pd.Series:
-            with torch.no_grad():
-                input = bc_tokenizer.value(texts.tolist(), return_tensors="pt", padding=True, truncation=True,
-                                           max_length=512)
-                all_embeddings = bc_model.value(input['input_ids'])['pooler_output'].tolist()
-                return pd.Series(all_embeddings)
+                @pandas_udf(T.ArrayType(T.DoubleType()))
+                def embed_batch_udf(texts: pd.Series) -> pd.Series:
+                    with torch.no_grad():
+                        input = bc_tokenizer.value(texts.tolist(), return_tensors="pt", padding=True, truncation=True,
+                                                   max_length=512)
+                        all_embeddings = bc_model.value(input['input_ids'])['pooler_output'].tolist()
+                        return pd.Series(all_embeddings)
 
-        schm = T.StructType.fromJson(json.load(open("/home/nir/spark_stuff/decahose_schema.json")))
-        schm.add("ds", T.StringType(), False, None)
-        decahose_tweets = spark.read.json("/user/nir/decahose_tweets", schema=schm)
-        decahose_tweets = decahose_tweets.filter(f"ds >= '{start_dt}' AND ds <= '{end_dt}'")
-        decahose_tweets = decahose_tweets.select("id", decahose_tweets.user.id.alias("user_id"), "text")
+                schm = T.StructType.fromJson(json.load(open("/home/nir/spark_stuff/decahose_schema.json")))
+                schm.add("ds", T.StringType(), False, None)
+                decahose_tweets = spark.read.json("/user/nir/decahose_tweets", schema=schm)
+                decahose_tweets = decahose_tweets.filter(f"ds == '{dt}'")
+                decahose_tweets = decahose_tweets.select("id", decahose_tweets.user.id.alias("user_id"), "text")
 
-        sample_tweets = decahose_tweets.join(followee_uids, on=F.col("followee_uid")==F.col("user_id")).drop("followee_uid")
+                sample_tweets = decahose_tweets.join(followee_uids, on=F.col("followee_uid")==F.col("user_id")).drop("followee_uid")
 
-        t0 = time.time()
+                t0 = time.time()
 
-        logging.info(f"Started embedding for start_dt={start_dt} and end_dt={end_dt}")
+                logging.info(f"Started embedding for dt={start_dt} and user_id_bucket in {str(i)}")
 
-        embed_df = sample_tweets.drop_duplicates().repartition(120, "user_id").withColumn("embed", embed_batch_udf(F.col('text'))).drop("text")
-        embed_df.write.save(
-            path="/user/etorf/followee_sample_embeddings", format="parquet", mode="append", compression="gzip")
+                embed_df = sample_tweets.drop_duplicates().repartition(120, "user_id").withColumn("embed", embed_batch_udf(F.col('text'))).drop("text")
+                embed_df.write.save(
+                    path="/user/etorf/followee_sample_embeddings", format="parquet", mode="append", compression="gzip")
 
-        logging.info(f"It took {time.time() - t0} seconds")
+                logging.info(f"Finished embedding. It took {time.time() - t0} seconds")
+
+                start_dt += datetime.timedelta(days=1) #add 1 day to continue to next day
 
         spark.stop()
 
